2d4894316be310eae58d4389d8e9cae37a4b8a75
unknown <jabri.oldd@gmail.com>
2022-08-27 16:48:12 +0100

    delete unpublished comments functionality

--- admin/deleteComment.php
+++ admin/deleteComment.php
@@ -1,1 +1,11 @@
 <?php
+include('./includes/header.php');
+
+$id = mysqli_escape_string($con, $_GET['id']);
+$query = "delete from comments where id='$id'";
+if (mysqli_query($con, $query)) {
+    $deleted = "deleted successfully";
+    header("location:comments.php?deleted=$deleted");
+} else {
+    echo 'error' . mysqli_error($con);
+}
